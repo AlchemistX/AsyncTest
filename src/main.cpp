474135cbfa2989f8d6dfe3e249633996492e71ef
AlchemistX <hick4321@gmail.com>
2019-09-16 10:38:27 +0900

    Adjust files

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
-#include "EventQueue.hpp"
-#include "Notification.hpp"
 
 int main(void)
 {
-  Notification n;
-  EventQueue e;
-  GDestroyNotify MarkDone = [](gpointer data) {
-        Notification* w = static_cast<Notification*>(data);
-        w->notify();
-      };
-  GSourceFunc Action = [](gpointer data) -> gboolean {
-        std::cout << "BAM!" << std::endl;
-        return FALSE;
-      };
-
-  uint32_t id = e.addTimeout(Action, 5000, &n, MarkDone);
-  
-  n.wait();
-  
   return 0;
 }
